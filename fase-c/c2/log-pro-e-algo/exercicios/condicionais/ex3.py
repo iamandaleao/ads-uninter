3ca483fd57f3273ba7468f4c5135233bbd4b2070
iamandaleao <amandabibiano144@gmail.com>
2025-10-23 17:17:28 -0300

    feat: add exercises

---
+++ fase-c/c2/log-pro-e-algo/exercicios/condicionais/ex3.py
@@ -0,0 +1,7 @@
+m1 = float(input('Digite a nota da 1º matéria: '))
+m2 = float(input('Digite a nota da 2ª matéria: '))
+m3 = float(input('Digite a nota da 3ª matéria: '))
+if m1 >= 7 and m2 >= 7 and m3 >= 7:
+    print('O aluno está aprovado de ano!');
+else:
+    print('O aluno não passou de ano!');
