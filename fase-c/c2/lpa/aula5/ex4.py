25d66c1da6b878450e5e2755c8e596af208000c8
iamandaleao <amandabibiano144@gmail.com>
2025-11-07 18:44:49 -0300

    feat: add exercises

---
+++ fase-c/c2/lpa/aula5/ex4.py
@@ -0,0 +1,3 @@
+#Faça uma função lambda que recebe dois valores numéricos como parâmetro. Ao primeiro valor, sempre some 5. Em seguida, multiplique ambos e retorne o resultado.
+calc = lambda a,b: (a + 5) * b
+print(calc(5,10))
