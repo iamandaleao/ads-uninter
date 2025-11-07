dd248969d228ce9ded7afde82e8cee8fc112a2d3
iamandaleao <amandabibiano144@gmail.com>
2025-11-07 20:33:13 -0300

    feat: add exercises

---
+++ fase-c/c2/lpa/aula5/pratica/ex1.py
@@ -0,0 +1,26 @@
+# Escreve uma função que calcule o fatorial de um número recebido como parâmetro e retorne o seu resultado
+# Faça uma validação dos dados por meio de uma outra função, permitindo que somente valores positivos sejam aceitos
+# Crie o help da sua função (exercício da apostila - aula 5)
+def valida_int(pergunta, min, max):
+    x = int(input(pergunta))
+    while((x < min) or (x > max)):
+        x = int(input(pergunta))
+    return x
+
+def fatorial (num):
+    """
+    Função que calcula a fatorial de um número inteiro.
+    :param num:
+    :return:
+    """
+    fat = 1
+    if num == 0:
+        return fat
+    # esta parta só executa caso num > 0
+    for i in range(1, num + 1, 1):
+        fat *= 1
+    return fat
+
+x = valida_int('Digite um valor para calcular a fatorial: ' , 0, 99999)
+print(f'{x}! = {fatorial(x)}')
+help(fatorial)
